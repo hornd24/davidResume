d4e596f5d0e8c740e75134489ba49565dd2bea51
hornd24 <david.horn689@gmail.com>
2018-06-28 21:53:14 -0700

    changed title and added admin User for techcheck username and password

--- client/src/Views/Components/Header/Header.jsx
+++ client/src/Views/Components/Header/Header.jsx
@@ -24,17 +24,13 @@ class Header extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-12 col-xs-9">
+                    <div className="col-lg-12 col-xs-12">
 
 
                         <div className='headerDiv'>
            <h1 className='headerNameTxt'>David Horn </h1>
                         <div className='boxing'>
-<<<<<<< HEAD
                             <h3 className='headerTxt' >Full Stack Javascript Developer</h3>
-=======
-                            <h3 className='headerTxt' >Full Stack Developer</h3>
->>>>>>> 80a8f39592f7816553afddf5a61ec628882d211f
                             </div>
                         </div>
 
